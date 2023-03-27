1a985097981b8464f3d3117fd384164d45dde0fa
dmytro <tsekhandmytro@gmail.com>
2023-03-27 18:23:56 +0300

    header

--- script/script.js
+++ script/script.js
@@ -1,16 +1,11 @@
 const signOpen = document.querySelector('.open');
-const signClose = document.querySelector('.X');
+const signClose = document.querySelector('.close');
 const form_wrap = document.querySelector('.header_menu')
-let form1 = document.form1
 
-form1.onsubmit = function (e) {
-    e.preventDefault();
-    console.log('click')
-};
 
 signOpen.addEventListener('click', function () {
     form_wrap.classList.add('active_form')
 })
-signClose.addEventListener('click', function () {
-    form_wrap.classList.remove('active_form')
+signOpen.addEventListener('click', function () {
+    form_wrap.classList.remove('header_menu')
 })
